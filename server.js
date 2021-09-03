331d70daf9af8492e4563cf7bd3dc36bfde56022
James Broski <james.broski.5@gmail.com>
2021-09-02 19:26:07 -0600

    starting prompts with inquirer

--- server.js
+++ server.js
@@ -2,3 +2,62 @@ const express = require("express");
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Coding27!", //will need to change to your own mysql password
+  database: "employeeTracker_db",
+});
+
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateEmployee();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
+}
